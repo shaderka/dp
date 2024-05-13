02d5d9b9ec8ae1778ee1b89e6e72b6d501a6f835
shaderka <ivan.podolskiy.045@yandex.ru>
2024-05-13 23:24:41 +0300

    testlogs

--- src/app/profile/users/page.js
+++ src/app/profile/users/page.js
@@ -38,6 +38,7 @@ export default function UsersPage() {
 				<div className='max-w-[600px] mx-auto'>
 					<a
 						href={'https://dodo-shaderka.vercel.app/logs.txt'}
+						//href={'http://localhost:3000/logs.txt'}
 						download='логи'
 						className=' inline-flex gap-2 items-center text-white bg-primary hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center'
 					>
